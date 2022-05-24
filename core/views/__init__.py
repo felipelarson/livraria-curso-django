2789c3b0d6e1427bd6d932e468df8098a326afe9
Felipe Silveira <felipelarson@msn.com>
2022-05-24 18:14:07 +0100

    chore: organized files

---
+++ core/views/__init__.py
@@ -0,0 +1,5 @@
+from .tetesIniciais import teste
+from .categoriaClass import CategoriaView
+from .categoriaApiView import CategoriasList, CategoriaDetail
+from .categoriaGeneric import CategoriaListGeneric, CategoriaDetailGeneric
+from .categoria import CategoriaViewSet
