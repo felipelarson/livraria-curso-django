9f664ec38378911496de8514490c286247d65b13
Felipe Silveira <felipelarson@msn.com>
2022-05-24 20:59:21 +0100

    feat: created new fuction to show the return

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,8 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    ModelSerializer,
+    CharField,
+    SerializerMethodField,
+)
 
 from core.models import Autor, Categoria, Editora, Livro
 
@@ -15,6 +19,15 @@ class EditoraSerializer(ModelSerializer):
         fields = "__all__"
 
 
+class EditoraNestedSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = (
+            "nome",
+            "site",
+        )
+
+
 class AutorSerializer(ModelSerializer):
     class Meta:
         model = Autor
@@ -28,7 +41,18 @@ class LivroSerializer(ModelSerializer):
 
 
 class LivroDetailSerializer(ModelSerializer):
+    categoria = CharField(source="categoria.descricao")
+    editora = EditoraNestedSerializer()
+    autores = SerializerMethodField()
+
     class Meta:
         model = Livro
         fields = "__all__"
         depth = 1
+
+    def get_autores(self, instance):
+        nomes_autores = []
+        autores = instance.autores.get_queryset()
+        for autor in autores:
+            nomes_autores.append(autor.nome)
+        return nomes_autores
