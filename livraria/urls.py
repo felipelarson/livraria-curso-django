33e75fbfcc5620918fe8fb71e525f9d9667e017d
Felipe Silveira <felipelarson@msn.com>
2022-05-24 15:15:59 +0100

    feat: ceated crud with APIView

--- livraria/urls.py
+++ livraria/urls.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 from django.urls import path
 
 
-urlpatterns = [path("admin/", admin.site.urls), path("teste/", views.teste)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("teste/", views.teste),
+    path("categorias/", views.CategoriaView.as_view()),
+    path("categorias/<int:id>/", views.CategoriaView.as_view()),
+    path("categorias-apiview/", views.CategoriaView.as_view()),
+    path("categorias-apiview/<int:id>/", views.CategoriaDetail.as_view()),
+]
