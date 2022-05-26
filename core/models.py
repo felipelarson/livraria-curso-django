4ba78ae4ca935bcae66db2121d6307b2de110614
Felipe Silveira <felipelarson@msn.com>
2022-05-26 20:27:20 +0100

    chore: update models.py

--- core/models.py
+++ core/models.py
@@ -2,20 +2,14 @@ from django.db import models
 from django.db.models import F
 from django.contrib.auth.models import User
 
+from Editora.models import Editora
+
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=255)
 
     def __str__(self):
         return self.descricao
-
-
-class Editora(models.Model):
-    nome = models.CharField(max_length=255)
-    site = models.URLField()
-
-    def __str__(self):
-        return "%s (%s)" % (self.nome, self.site)
 
 
 class Autor(models.Model):
